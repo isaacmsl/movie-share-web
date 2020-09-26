5b58ca7b692c928d6c90ac9a40d9f44b2b107f61
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-09-26 16:50:22 -0300

    feat: ++notifications

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const debug = require('debug')('server')
 
 const app = express()
 const server = http.createServer(app)
@@ -8,8 +9,45 @@ const io = socketio(server)
 
 app.use(express.static('public'))
 
+let currentUsers = new Map()
+let isPaused = true
+
 io.on('connection', (socket) => {
+    socket.on('setUsername', username => {
+        let thereIsUsername = false
+        currentUsers.forEach(existingUsername => {
+            if (existingUsername === username) {
+                thereIsUsername = true
+                socket.emit('existingUsername')
+                return
+            }
+        })
+        
+        if (thereIsUsername) return
+
+        debug('new user -> ' + username)
+        currentUsers.set(socket.id, username)
+        socket.emit('canWatch', isPaused)
+        io.emit('broadcast', 'newUser', username)
+
+    })
+
+    socket.on('disconnect', () => {
+        const username = currentUsers.get(socket.id)
+        if (username) {
+            debug('user disconnected -> ' + username)
+            io.emit('broadcast', 'userDisconnected', username)
+            currentUsers.delete(socket.id)
+        }
+    })
+
     socket.on('action', (action, value) => {
+        if (action === 'pause') {
+            isPaused = true
+        } else if (action === 'play') {
+            isPaused = false
+        }
+
         io.emit('broadcast', action, value)
     })
 })
